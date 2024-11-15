78bb61c25fa10589bd9d2d217bc4ca711fb53a5e
Gabriel <paschoalsoftwareengineer@gmail.com>
2024-11-14 21:23:08 -0300

    feature: create migrations

---
+++ src/entities/User.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('users')
+export class User {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({type: 'text'})
+    name: string;
+
+    @Column({type: 'text', unique: true})
+    email: string;
+
+    @Column({type: 'text'})
+    password: string;
+
+    constructor(name: string, email: string, password: string) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
